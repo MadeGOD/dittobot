3e38bf9db56bea6b5cb0c6961839c72655cedec6
ditto7890 <kwak071219@gmail.com>
2020-05-23 15:06:08 +0900

    update

--- commands/crawling/github.js
+++ commands/crawling/github.js
@@ -8,7 +8,7 @@ module.exports = {
     run: async (client, message, args, ops) => {
         if (!args.join(' ')) return;
 
-        const { login, avatar_url, location, created_at, followers, following, email, blog, html_url, bio, public_repos, public_gists } = await fetch(`https://api.github.com/users/${encodeURI(args.join(" "))}`).then(e => e.json());
+        const { login, avatar_url, location, created_at, followers, following, email, blog, html_url, bio, public_repos, public_gists, type } = await fetch(`https://api.github.com/users/${encodeURI(args.join(" "))}`).then(e => e.json());
 
         if (!login) return message.channel.send(`\`${args.join(" ")}\` (이)라는 유저를 찾을 수 없습니다...`);
 
@@ -16,14 +16,20 @@ module.exports = {
         .addFields([
             {name: "이름", value: `**${login}**`, inline: true},
             {name: "상태 메세지", value: `**${bio ? bio : "없음"}**`},
-            {name: "지역", value: `**${location ? location : "없음"}**`},
+            {name: "지역", value: `**${location ? location : "없음"}**`, inline: true},
+            {name: '계정', value: `**${accountType[type]}**`},
             {name: "레포지토리", value: `**${public_repos ? `${public_repos}개` : "없음"}**`, inline: true},
             {name: "Gist", value: `**${public_gists ? `${public_gists}개` : "없음"}**`, inline: true},
-            {name: "블로그", value: `**${blog ? blog : "없음"}**`},
+            {name: "블로그", value: `**${blog ? blog : "없음"}**`, inline: true},
+            {name: "이메일", value: `**${email ? email : "없음"}**`, inline: true},
             {name: "팔로워", value: `**${followers ? `${followers}명` : "없음"}**`, inline: true},
             {name: "팔로잉", value: `**${following ? `${following}명` : "없음"}**`, inline: true},
-            {name: "가입 날짜", value: `**${ops.formatTime(created_at)}**`},
-            {name: "이메일", value: `**${email ? email : "없음"}**`, inline: true}
+            {name: "가입 날짜", value: `**${ops.formatTime(created_at)}**`}
         ]))
     }
+}
+
+const accountType = {
+    User: '유저',
+    Organization: '공식'
 }
