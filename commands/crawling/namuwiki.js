85ca8353d78480e0ce620a147b3ef6d9a12dbedb
ditto7890 <kwak071219@gmail.com>
2020-05-23 17:33:09 +0900

    update

--- commands/crawling/namuwiki.js
+++ commands/crawling/namuwiki.js
@@ -12,25 +12,15 @@ module.exports = {
         await axios.get(`https://namu.wiki/Search?q=${encodeURI(args.join(" "))}`).then(res => {
             if (res.status !== 200) return message.channel.send('정보를 불러올 수 없습니다.');
 
-            let result = {
-                result: [],
-                goTo: [],
-                description: []
-            };
-
             const $ = cheerio.load(res.data);
-
-            $('div.search-item').each((i, element) => {
-                result['result'][i] = $(element).find('h4 > a').text().trim();
-                result['goTo'][i] = `https://namu.wiki${$(element).find('h4 > a').attr('href')}`;
-                result['description'][i] = $(element).find('div').text().trim()
-            });
 
             let str = ``;
 
-            for (var i = 0; i < result.result.length; i++) {
-                str += `[${result.result[i]}](${result.goTo[i]})\n`
-            };
+            $('div.search-item').each((i, element) => {
+                str += `[${$(element).find('h4 > a').text().trim()}](https://namu.wiki${$(element).find('h4 > a').attr('href')})`
+            });
+
+            if (!str) return message.channel.send('검색 결과가 없습니다')
     
             message.channel.send(new MessageEmbed().setTitle('꺼무위키 검색 결과').setDescription(str).setColor(0x008275))
         })
