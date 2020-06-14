8709f94d2e369ca3c39438af46422bf2a1f5d2f6
ditto7890 <kwak071219@gmail.com>
2020-06-14 11:00:29 +0900

    update

--- commands/command/translate.js
+++ commands/command/translate.js
@@ -11,13 +11,13 @@ module.exports = {
             if (!args[1]) return message.channel.send('번역 언어를 입력해 주세요')
             if (!args.slice(2).join(" ")) return message.channel.send('번역할 메세지를 입력해 주세요')
 
-            translate(args.slice(2).join(" "), { to: args[1] }).then(r => message.channel.send(new MessageEmbed().setAuthor('Google Translate', 'https://upload.wikimedia.org/wikipedia/commons/d/db/Google_Translate_Icon.png', 'https://translate.google.co.kr/').setTitle('구글 번역').setColor(0x00ff00).setDescription(`**${res.from.language.iso}** -> **${args[1]}**\n\n**번역 전**\n\`\`\`fix\n${args.slice(2).join(" ")}\n\`\`\`\n**번역 후**\n\`\`\`yml\n${r.text}\n\`\`\``))).catch(e => message.channel.send(`구글 번역기에서 지원하지 않은 언어이거나, 알 수 없는 오류가 발생했습니다\n${e}`))
+            translate(args.slice(2).join(" "), { to: args[1] }).then(r => message.channel.send(new MessageEmbed().setAuthor('Google Translate', 'https://upload.wikimedia.org/wikipedia/commons/d/db/Google_Translate_Icon.png', 'https://translate.google.co.kr/').setTitle('구글 번역').setColor(0x00ff00).setDescription(`**${res.from.language.iso}** -> **${args[1]}**\n\n**번역 전**\n\`\`\`fix\n${args.slice(2).join(" ")}\n\`\`\`\n**번역 후**\n\`\`\`yml\n${r.text}\n\`\`\``).setFooter('https://github.com/Pneuma714/Pod/blob/master/commands/utilities/translate.js'))).catch(e => message.channel.send(`구글 번역기에서 지원하지 않은 언어이거나, 알 수 없는 오류가 발생했습니다\n${e}`))
         } else {
             if (!args[0]) return message.channel.send('시작 언어를 입력해 주세요')
             if (!args[1]) return message.channel.send('번역 언어를 입력해 주세요')
             if (!args.slice(2).join(" ")) return message.channel.send('번역할 메세지를 입력해 주세요')
 
-            translate(args.slice(2).join(" "), { from: args[0], to: args[1] }).then(r => message.channel.send(new MessageEmbed().setAuthor('Google Translate', 'https://upload.wikimedia.org/wikipedia/commons/d/db/Google_Translate_Icon.png', 'https://translate.google.co.kr/').setTitle('구글 번역').setColor(0x00ff00).setDescription(`**${args[0]}** -> **${args[1]}**\n\n**번역 전**\n\`\`\`fix\n${args.slice(2).join(" ")}\n\`\`\`\n**번역 후**\n\`\`\`yml\n${r.text}\n\`\`\``))).catch(e => message.channel.send(`구글 번역기에서 지원하지 않은 언어이거나, 알 수 없는 오류가 발생했습니다\n${e}`))
+            translate(args.slice(2).join(" "), { from: args[0], to: args[1] }).then(r => message.channel.send(new MessageEmbed().setAuthor('Google Translate', 'https://upload.wikimedia.org/wikipedia/commons/d/db/Google_Translate_Icon.png', 'https://translate.google.co.kr/').setTitle('구글 번역').setColor(0x00ff00).setDescription(`**${args[0]}** -> **${args[1]}**\n\n**번역 전**\n\`\`\`fix\n${args.slice(2).join(" ")}\n\`\`\`\n**번역 후**\n\`\`\`yml\n${r.text}\n\`\`\``).setFooter('https://github.com/Pneuma714/Pod/blob/master/commands/utilities/translate.js'))).catch(e => message.channel.send(`구글 번역기에서 지원하지 않은 언어이거나, 알 수 없는 오류가 발생했습니다\n${e}`))
         }
     }
 }
