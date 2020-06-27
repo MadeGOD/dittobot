d32239a8fde701e22a2fcd5713cf0a19cea45f03
ditto7890 <kwak071219@gmail.com>
2020-06-27 21:57:02 +0900

    update

--- commands/owner/shell.js
+++ commands/owner/shell.js
@@ -1,4 +1,5 @@
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js"),
+    { exec } = require("child_process")
 
 module.exports = {
     name: "shell",
@@ -7,7 +8,7 @@ module.exports = {
     run: async (client, message, args) => {
         if (!args.join(" ")) return
 
-        require("child_process").exec(args.join(" "), (err, stdout, stderr) => {
+        exec(args.join(" "), (err, stdout, stderr) => {
             if (err) message.channel.send(new MessageEmbed().setTitle("Shell").setColor(0xff0000).setDescription(`**📥 Input: **\n\`\`\`sh\n${args.join(" ")}\n\`\`\`\n**📤 Output: **\n\`\`\`sh\n${stderr.length > 1500 ? stderr.substr(0, 1500) : stderr}\n\`\`\``))
             else message.channel.send(new MessageEmbed().setTitle("Shell").setColor(0x00ff00).setDescription(`**📥 Input: **\n\`\`\`sh\n${args.join(" ")}\n\`\`\`\n**📤 Output: **\n\`\`\`sh\n${stdout.length > 1500 ? stdout.substr(0, 1500) : stdout}\n\`\`\``))
         })
