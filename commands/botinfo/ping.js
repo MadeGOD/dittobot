4b4c7846e74d8f5c731a45d743c588f1e4315a7c
ditto7890 <kwak071219@gmail.com>
2020-07-04 22:38:02 +0900

    update

--- commands/botinfo/ping.js
+++ commands/botinfo/ping.js
@@ -6,6 +6,6 @@ module.exports = {
     category: "botinfo",
     run: async (client, message, args) => {
         const m = await message.channel.send("🏓 **Pinging...**")
-        m.edit("", new MessageEmbed().setTitle("🏓 Pong!").setColor(0xffff00).setFooter(message.author.username, message.author.displayAvatarURL()).setTimestamp().setDescription(`**Discord API Latency**\n${client.ws.ping}ms\n\n**Latency**\n${m.createdAt - message.createdAt}ms`))
+        m.edit("", new MessageEmbed().setTitle("🏓 Pong!").setColor(0x00ff00).setFooter(message.author.username, message.author.displayAvatarURL()).setTimestamp().setDescription(`**Discord API Latency**\n${client.ws.ping}ms\n\n**Latency**\n${m.createdAt - message.createdAt}ms`))
     }
 }
