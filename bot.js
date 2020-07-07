d2c41ee5f5accbd27e1c3b5c29edd9a96c618d7e
ditto7890 <kwak071219@gmail.com>
2020-07-08 08:54:20 +0900

    update

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on("ready", () => {
 
 	setInterval(() => client.user.setActivity(activity[Math.floor(Math.random() * activity.length)]), 10000)
 
-	ops.MyBot.update(client.guilds.cache.size).then(e => console.log(e.code)).catch(e => console.error(e.message))
+	//ops.MyBot.update(client.guilds.cache.size).then(e => console.log(e.code)).catch(e => console.error(e.message))
 
 	client.musicManager = new(require("./structures/MusicManager"))(client)
 })
