1bb320884967dcb43e1c543c9111448382692a00
ditto7890 <kwak071219@gmail.com>
2020-07-12 12:15:20 +0900

    update

--- commands/naver/movie.js
+++ commands/naver/movie.js
@@ -20,6 +20,6 @@ module.exports = {
 
         const { items: [ { title, subtitle, link, image, pubDate, director, actor, userRating } ] } = res
 
-        message.channel.send(new MessageEmbed().setAuthor('Naver Movie', 'https://www.naver.com/favicon.ico?1', 'https://movie.naver.com/').setTitle('네이버 영화').setURL(link).setThumbnail(image).setColor(0x00ff00).setDescription(`**${title.replace(/<b>/g, '').replace(/<\/b>/g, '')}**\n${subtitle && `(${subtitle.replace(/<b>/g, '').replace(/<\/b>/g, '')})\n`}\n제작연도: ${pubDate}\n감독: ${director.split('|').slice(0, -1).join(', ') || "알 수 없음"}\n배우: ${actor.split('|').slice(0, -1).join(', ') || "알 수 없음"}\n평점: ${parseFloat(userRating) || "없음"}`))
+        message.channel.send(new MessageEmbed().setAuthor('Naver Movie', 'https://www.naver.com/favicon.ico?1', 'https://movie.naver.com/').setTitle('네이버 영화').setURL(link).setThumbnail(image).setColor(0x00ff00).setDescription(`**${title.replace(/<b>/g, '').replace(/<\/b>/g, '')}**\n${subtitle && `(${subtitle.replace(/<b>/g, '').replace(/<\/b>/g, '')})\n`}\n**제작연도**: ${pubDate}\n**감독**: ${director.split('|').slice(0, -1).join(', ') || "알 수 없음"}\n**배우**: ${actor.split('|').slice(0, -1).join(', ') || "알 수 없음"}\n**평점**: ${parseFloat(userRating) || "없음"}`))
     }
 }
